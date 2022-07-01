4f1a90cc936e028a4719c33dc0b2301eec6b1cb8
Viviane <vivianedasilvasouza2020@gmail.com>
2022-07-01 08:06:11 -0300

    feat: estilização da home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,4 @@
 <template>
-  <Banner />
   <div class="home">
     <div class="products">
       <div
@@ -59,23 +58,12 @@ export default {
 
     .product {
       flex: 0 0 30%;
+      display: grid;
       box-sizing: border-box;
       box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
       padding: 16px;
       margin: 8px;
-      height: 360px;
-      @media only screen and (max-width: 769px) {
-        flex: 0 0 40%;
-      }
-
-      @media only screen and (max-width: 640px) {
-        flex: 0 0 90%;
-      }
-
-      &.inBag {
-        border: 1px solid #007bff;
-      }
-
+      
       .product-image {
         margin: 20px auto;
         width: 160px;
@@ -91,16 +79,16 @@ export default {
         max-width: 60%;
         font-weight: normal;
       }
-
       p.price {
         font-size: 20px;
         font-weight: bold;
       }
       .button {
         background-color:  rgb(212, 16, 147); 
-        border: none;
+        border-radius: 100px;
         color: white;
-        padding: 15px 32px;
+        border: none;
+        padding: 9px 19px;
         text-align: center;
         text-decoration: none;
         display: inline-block;
@@ -113,28 +101,6 @@ export default {
       .button2:hover {
         box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
 }
-
-      button {
-        color: #fff;
-        background-color: #007bff;
-        border: 1px solid #007bff;
-        border-radius: 100px;
-        font-weight: 400;
-        text-align: center;
-        padding: 8px 16px;
-        cursor: pointer;
-
-        &:hover {
-          opacity: 0.8;
-        }
-
-        &.remove {
-          background-color: transparent;
-          border: none;
-          color: black;
-          text-decoration: underline;
-        }
-      }
     }
   }
 }
